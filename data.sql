2e91702175ba3601bc8b6ad02fb23966f78b4877
mnaibei <muchajulius@gmail.com>
2023-06-05 15:07:07 +0300

    Made requested changes

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES('Agumon', 03/02/2020, 0, true, 10.23);
-VALUES( 'Gabumon', 15/11/2018, 2, true, 8);
-VALUES('Pikachu', 07/01/2021, 1, false, 15.04); 
-VALUES('Devimon', 12/05/2017, 5, true, 11);
+VALUES('Agumon', 03/02/2020, 0, true,10.23);
+VALUES( 'Gabumon', 15/11/2018, 2, true,8);
+VALUES('Pikachu', 07/01/2021, 1, false,15.04); 
+VALUES('Devimon', 12/05/2017, 5, true,11);
--- schema.sql
+++ schema.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE animals(
   id INT GENERATED ALWAYS AS IDENTITY, 
-  name VARCHAR(250), 
-  date_of_birth DATE, 
-  escape_attempts INT, 
-  neutered BOOLEAN, 
-  weight_kg DECIMAL
+  name VARCHAR(250),  
+  date_of_birth DATE,  
+  escape_attempts INT,  
+  neutered BOOLEAN,  
+  weight_kg DECIMAL 
   );
