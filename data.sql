3e02d697625164d2e652cc1cc45d6931b48f877d
mnaibei <muchajulius@gmail.com>
2023-06-06 12:52:52 +0300

    Performed queries and updates to table

--- data.sql
+++ data.sql
@@ -1,7 +1,14 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES('Agumon', 03/02/2020, 0, true,10.23);
-VALUES( 'Gabumon', 15/11/2018, 2, true,8);
-VALUES('Pikachu', 07/01/2021, 1, false,15.04); 
-VALUES('Devimon', 12/05/2017, 5, true,11);
+VALUES('Agumon', '03/02/2020', 0, true,10.23),
+( 'Gabumon', '15/11/2018', 2, true,8),
+('Pikachu', '07/01/2021', 1, false,15.04), 
+('Devimon', '12/05/2017', 5, true,11),
+('Charmander', '2020-02-08', 0, false,-11),
+('Plantmom', '2021-11-15', 2, true,-5.7),
+('Squirtle', '1993-04-02', 3, false, -12.3), 
+('Angemon', '2005-07-12', 1, true, -45), 
+('Boarmon', '2005-07-07', 7, true, 20.04),
+('Blossom', '1998-10-13', 3, true, 17),
+('Ditto', '2022-05-14', 4, true, 22);
--- queries.sql
+++ queries.sql
@@ -9,3 +9,70 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE; 
 SELECT * FROM animals WHERE name <> 'Gabumon'; 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3; 
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT new_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO new_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+ count 
+-------
+    10
+(1 row)
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+ count 
+-------
+     2
+(1 row)
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+         avg         
+---------------------
+ 15.5310000000000000
+(1 row)
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) AS total_escapes FROM animals 
+GROUP BY neutered
+ORDER BY total_escapes DESC;
+ neutered | total_escapes 
+----------+---------------
+ t        |            20
+ f        |             4
+(2 rows)
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals
+GROUP BY species;
+ species | min | max 
+---------+-----+-----
+ pokemon | 5.7 |  17
+ digimon |   8 |  45
+(2 rows)
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+ species |        avg         
+---------+--------------------
+ pokemon | 3.0000000000000000
